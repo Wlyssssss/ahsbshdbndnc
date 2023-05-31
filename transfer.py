7508c028e0e91b789992243024c9024ae13626a0
yyk19 <yark123456789@163.com>
2023-05-31 15:32:35 +0000

    supplement some other configs.

--- transfer.py
+++ transfer.py
@@ -2,11 +2,11 @@ from omegaconf import OmegaConf
 from scripts.rendertext_tool import Render_Text, load_model_from_config
 import torch
 
-# cfg = OmegaConf.load("config_ema.yaml")
+# cfg = OmegaConf.load("other_configs/config_ema.yaml")
 # # model = load_model_from_config(cfg, "model_states.pt", verbose=True)
 # model = load_model_from_config(cfg, "mp_rank_00_model_states.pt", verbose=True)
 
-cfg = OmegaConf.load("config_ema_unlock.yaml")
+cfg = OmegaConf.load("other_configs/config_ema_unlock.yaml")
 epoch_idx = 39
 model = load_model_from_config(cfg, "epoch={:0>6d}.ckpt".format(epoch_idx), verbose=True)
 
